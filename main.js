d9d9284a501b0b5f33103ea31225344f746f49bc
msoderblom <soderblom.matilda@gmail.com>
2020-02-07 13:55:19 +0100

    Added totalAmount function

--- main.js
+++ main.js
@@ -8,19 +8,26 @@ $(document).ready(function() {
   let p_container = $("#product_container");
   let shoppingCart = $("#shoppingCart");
 
-  console.log(localStorage.cartList);
+  $("#emptyCartBtn").click(emptyCart);
 
-  if (typeof localStorage.cartList != "undefined") {
+  // console.log(localStorage.cartList);
+
+  if (
+    typeof localStorage.cartList != "undefined" &&
+    localStorage.cartList.length != 0
+  ) {
     cartList = JSON.parse(localStorage.getItem("cartList"));
     drawCart();
   }
+
+  totalAmount();
 
   fetch("products.json")
     .then(resp => resp.json())
     .then(productsObj => {
       productList = productsObj.products;
       productList.forEach(product => {
-        console.log(product.name);
+        // console.log(product.name);
         let product_card = `
         <div>
         <div class="uk-card uk-card-default" id="card">
@@ -36,20 +43,13 @@ $(document).ready(function() {
 
           </p>
           <div class="uk-margin">
-           <label class="uk-form-label" for="form-stacked-select">Select</label>
+           <label class="uk-form-label" for="form-stacked-select">Quantity</label>
             <div class="uk-form-controls">
-             <select class="uk-select" id="form-stacked-select">
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                  <option>6</option>
-                  <option>7</option>
-                  <option>8</option>
-                  <option>9</option>
-                  <option>10</option>
-              </select>
+       
+              <div class="uk-margin">
+                <input data-inputid="${product.id}" class="uk-number" min="1" max="10"type="number" value="1">
+              </div>
+              
             </div>
           </div>
           <button data-product-id="${product.id}"class="uk-button uk-button-default addBtn">Add To Cart</button>
@@ -65,13 +65,31 @@ $(document).ready(function() {
 
   function addToCart(event) {
     let productId = event.currentTarget.dataset.productId;
+    let qtyInput = $(`[data-inputid= '${productId}']`)[0];
+    let qty = qtyInput.value;
+
+    // console.log(qtyInput.dataset.inputid);
+    console.log("värdet från input: " + qtyInput.value);
+
     productList.forEach(product => {
       if (product.id == productId) {
         cartList.push(product);
+        cartList.forEach(product => {
+          if (product.id == productId) {
+            product.qty = qty;
+            console.log("produktens gty-egenskap" + product.qty);
+
+            product.price = product.price * product.qty;
+            console.log("produktens pris" + product.price);
+          }
+        });
+
         drawCart();
         updateLocalStorage();
       }
     });
+
+    totalAmount();
   }
   function removeFromCart(event) {
     let productId = event.currentTarget.dataset.id;
@@ -83,20 +101,23 @@ $(document).ready(function() {
       }
     });
   }
+  //
   function drawCart() {
     shoppingCart[0].innerHTML = "";
     cartList.forEach(product => {
       let cartCard = `
-  
+      
   <div class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid>
     <div class="uk-card-media-left uk-cover-container">
+    <canvas width="150" height="150"></canvas>
         <img src="${product.img}" alt="" uk-cover>
-        <canvas width="600" height="200"></canvas>
+        
     </div>
     <div>
         <div class="uk-card-body">
         <span data-id="${product.id}" class="deleteBtn" uk-icon="icon: trash; ratio: 2"></span>
             <h3 class="uk-card-title">${product.name}</h3>
+            <h2>Qty: ${product.qty}</h2>
             <h2>${product.price} kr</h2>
         </div>
     </div>
@@ -106,12 +127,31 @@ $(document).ready(function() {
       shoppingCart.append(cartCard);
     });
     $(".deleteBtn").click(removeFromCart);
-    console.log(cartList);
+    // console.log(cartList);
+  }
+
+  function totalAmount() {
+    let totalAmount = 0;
+
+    if (cartList.length != 0) {
+      cartList.forEach(product => {
+        totalAmount += product.price;
+      });
+    }
+
+    $("#totalAmount").text(totalAmount + " kr");
   }
 
   function updateLocalStorage() {
     localStorage.setItem("cartList", JSON.stringify(cartList));
-    console.table(localStorage);
+    // console.table(localStorage);
+  }
+
+  function emptyCart() {
+    localStorage.cartList = "";
+    cartList = [];
+    drawCart();
+    totalAmount();
   }
 });
 
